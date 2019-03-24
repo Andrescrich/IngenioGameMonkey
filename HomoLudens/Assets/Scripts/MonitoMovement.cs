233aec06a4d0f5c7fd238822e779fcb4409963f1
Stéphane Díaz-Alejo León <ragnarokshadows99@gmail.com>
2019-03-24 15:53:38 +0100

    First steps

---
+++ HomoLudens/Assets/Scripts/MonitoMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonitoMovement : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private float velocity = 4f;
+    private bool jump = false;
+    private float jumpForce = 800f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Movimiento hacia la derecha
+        rb.velocity = new Vector3(velocity, rb.velocity.y, 0);
+
+        //Salto
+        if (jump) rb.AddForce(new Vector2(0f, jumpForce));
+        jump = false;
+    }
+}
