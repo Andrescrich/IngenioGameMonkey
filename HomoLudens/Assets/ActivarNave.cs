2f8569b92022b09a4aec582f0182f52ca02779fa
André <andrescrich@hotmail.com>
2019-03-25 00:34:21 +0100

    Movida con branches panic over

---
+++ HomoLudens/Assets/ActivarNave.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivarNave : MonoBehaviour
+{
+    private GameObject player;
+    private GameObject nave;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        nave = GameObject.FindGameObjectWithTag("Nave");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject == player)
+        {
+            MonitoMovement.velocity = 0f;
+            GameManagerScript.inputEnabled = false;
+            player.transform.position = new Vector3(39.2f, -4.7f, 0f);
+            Invoke("ActivaNave", 2f);
+        }
+    }
+
+
+    void ActivaNave()
+    {
+        nave.GetComponent<naveScript>().enabled = true;
+    }
+}
---
+++ HomoLudens/Assets/GameManagerScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public static bool inputEnabled;
+
+    private void Start()
+    {
+        inputEnabled = true;
+    }
+}
